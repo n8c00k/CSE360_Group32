d1fd65c8e0a75298eddf8216c184f5ed6c33fd33
Ruktismo <68973748+Ruktismo@users.noreply.github.com>
2022-03-05 17:35:24 -0700

    Meeting 3-5, and Context updated
    Functions for context added but commented out until data classes are added.

--- CSE360Project/src/SQLite_db/Context.java
+++ CSE360Project/src/SQLite_db/Context.java
@@ -13,16 +13,23 @@ package SQLite_db;
 import java.io.File;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class Context {
+	/*
+	 * NOTES
+	 * menu and fooditem need a name field
+	 * user needs a payment var
+	 */
 	
 	//connection obj that all sqlite interactions are handled through
 	private Connection conn = null;
 	
 	public Context() {
 		try {
-			Class.forName("org.sqlite.JDBC");//for some reason this makes it able to fin d the database
+			Class.forName("org.sqlite.JDBC");//for some reason this makes it able to find the database
 			//get file path for workspace
 			File dbfile=new File(".");//dot is relative to current directory
 			//Drive Manager parameters
@@ -35,6 +42,72 @@ public class Context {
         } catch (ClassNotFoundException e) {
         	 System.out.println(e.getMessage());
 		}
+	}
+	/* REMOVE WHEN DATA CALSSES ARE ADDED
+	//Adds/updates a user to the database.
+	public void setUser(User u) {
+		
+	}
+	
+	//Adds/updates payment info for a user.
+	public void setPaymentInfo(User u) {
+		
+	}
+	
+	//Adds/updates a food item to the database
+	public void setFoodItem(FoodItem f) {
+		
+	}
+	
+	//Adds/updates a food item to the database
+	public void addCoupon(Coupon c, User u) {
+		
+	}
+	
+	//Deletes a coupon from the database
+	public void removeCoupon(Coupon c, User u) {
+		
+	}
+	
+	//Returns a new user obj from the database, searched by email. 
+	public User getUser(String email) {
+		
+	}
+	
+	//Returns a new menu obj from the database, searched by name.
+	public Menu getMenu(String menuName) {
+		
+	}
+	
+	//Returns a new food item obj from the database, searched by name.
+	public FoodItem getFoodItem(String name) {
+		
+	}
+	
+	//Returns a payment info obj, searched by user.
+	public Payment getPaymentInfo(User u) {
+		
+	}*/
+	
+	private void setData(String stmt) throws Exception {
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(stmt);
+			pstmt.executeUpdate();
+		} catch (SQLException e) {
+			throw new Exception("ERROR: SQL Statement failed\nQuerry: "+stmt+"\n\nFull Trace:\n"+e.getMessage());
+		}
+		return;
+	}
+	
+	private ResultSet getData(String stmt) throws Exception {
+		ResultSet r;
+		try {
+			PreparedStatement pstmt = conn.prepareStatement(stmt);
+			r = pstmt.executeQuery();
+		} catch (SQLException e) {
+			throw new Exception("ERROR: SQL Statement failed\nQuerry: "+stmt+"\n\nFull Trace:\n"+e.getMessage());
+		}
+		return r;
 	}
 
 }
