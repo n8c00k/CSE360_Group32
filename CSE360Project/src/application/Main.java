9409521d14e7a1bcf83f792e7fa77748889dc119
johnavarro1 <98787399+johnavarro1@users.noreply.github.com>
2022-03-22 18:51:41 -0700

    main change

--- CSE360Project/src/application/Main.java
+++ CSE360Project/src/application/Main.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import SQLite_db.Context;
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
@@ -19,8 +20,8 @@ public class Main extends Application {
 		stg = primaryStage;
 		primaryStage.setResizable(false);
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("/application/Sample.fxml"));
-			Scene scene = new Scene(root,400,400);
+			Parent root = FXMLLoader.load(getClass().getResource("/application/existingUserScene.fxml"));
+			Scene scene = new Scene(root,400,600);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
--- CSE360Project/src/module-info.java
+++ CSE360Project/src/module-info.java
@@ -3,6 +3,7 @@ module CSE360Project {
 	requires javafx.fxml;
 	requires java.sql;
 	requires javafx.base;
+	requires javafx.graphics;
 	
 	opens application to javafx.graphics, javafx.fxml;
 }
