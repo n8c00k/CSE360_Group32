8d59004ba852085b21ba0e9e8ffa2fa6bc5692e7
SopAlc23 <98788910+SopAlc23@users.noreply.github.com>
2022-03-23 20:24:29 -0700

    Update Menu.java

--- CSE360Project/src/application/dataObjects/Menu.java
+++ CSE360Project/src/application/dataObjects/Menu.java
@@ -1,12 +1,15 @@
 package application.dataObjects;
 
+import java.util.ArrayList;
+
 public class Menu {
     
-    public foodItem[] foods;
+	public ArrayList<foodItem> foods;
+    //public foodItem[] foods;
     public String menuName;
     
     public void addFoodItem(foodItem food){
-    	foods[foods.length] = food;
+    	//foods[foods.length] = food;
     }
     public void removeFoodItem(){
         
