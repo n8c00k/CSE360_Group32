795524e5c93252ae6f222f6c957a04f69149ce14
SopAlc23 <98788910+SopAlc23@users.noreply.github.com>
2022-03-23 23:17:32 -0700

    Update Customer.java

--- CSE360Project/src/application/dataObjects/Customer.java
+++ CSE360Project/src/application/dataObjects/Customer.java
@@ -5,9 +5,9 @@ import java.util.ArrayList;
 public class Customer extends User {
 
     
-	public Cart cart;
-    public static ArrayList<Coupon> coupons;
-    public static Integer pastOrders;
+	public ArrayList<Cart> cart;
+    public ArrayList<Coupon> coupons;
+    public Integer pastOrders;
     public Payment card;
     
     public Customer( String name, String email, String password) {
@@ -15,12 +15,51 @@ public class Customer extends User {
     	pastOrders = 0;
     	coupons = new ArrayList<Coupon>();
     }
-
     
-
-    public void paymentInfo(){
-
+    public void setCart(ArrayList<Cart> cart) {
+    	  this.cart = cart;
+    	}
+    
+    public ArrayList<Cart> getCart() {
+        return cart;
     }
+    
+    public Cart getCartIndex(int i) {
+    	return cart.get(i);
+    }
+
+    public void setCoupon(ArrayList<Coupon> coupon) {
+  	  this.coupons = coupon;
+  	}
+    
+    public ArrayList<Coupon> getCoupon() {
+        return coupons;
+    }
+
+    public Coupon getCouponIndex(int i) {
+    	return coupons.get(i);
+    }
+    
+    public void setPastOrders(Integer orders){
+    	this.pastOrders = orders;
+    }
+    
+    public Integer getPastOrders() {
+    	return pastOrders;
+    }
+    
+    public void setCard(Payment newCard) {
+    	this.card = newCard;
+    }
+    
+    public Payment getCard() {
+    	return card;
+    }
+    
+    //Do we need this method anymore?
+    ///public void paymentInfo(){
+
+   /// }
 
 	@Override
 	public String toString() {
