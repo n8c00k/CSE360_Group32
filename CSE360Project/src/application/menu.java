8f3d575e2f5b45fe9b7534af2d00409072ef2d7d
johnavarro1 <98787399+johnavarro1@users.noreply.github.com>
2022-03-25 19:09:08 -0700

    menu controller

---
+++ CSE360Project/src/application/menu.java
@@ -0,0 +1,29 @@
+package application;
+
+import application.dataObjects.User;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class menu {
+	
+	
+	private User user;
+	
+	public menu() {
+		
+	}
+	
+	
+	@FXML
+	private Button search;
+	@FXML
+	private Button cart;
+	@FXML
+	private Button account;
+	@FXML
+	private Button logout;
+	@FXML
+	private TextField searchItem;
+	
+	
+}
