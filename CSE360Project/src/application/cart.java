772cc9293c635f61a10ba664b4bb69d4d92a9de6
SopAlc23 <98788910+SopAlc23@users.noreply.github.com>
2022-03-25 19:41:53 -0700

    Controller Added

---
+++ CSE360Project/src/application/cart.java
@@ -0,0 +1,72 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class cart {
+	public cart() {
+		
+	}
+	@FXML 
+	private Button menu;
+	
+	@FXML 
+	private Button account;
+	
+	@FXML 
+	private Button remove;
+	
+	@FXML 
+	private Label item1;
+	
+	@FXML 
+	private Label item2;
+	
+	@FXML 
+	private Label item3;
+	
+	@FXML 
+	private Button addItem1;
+	
+	@FXML 
+	private Button removeItem1;
+	
+	@FXML 
+	private Button addItem2;
+	
+	@FXML 
+	private Button removeItem2;
+	
+	@FXML 
+	private Button addItem3;
+	
+	@FXML 
+	private Button removeItem3;
+	
+	@FXML 
+	private TextField quantityItem1;
+	
+	@FXML 
+	private TextField priceItem1;
+	
+	@FXML 
+	private TextField quantityItem2;
+	
+	@FXML 
+	private TextField priceItem2;
+	
+	@FXML 
+	private TextField quantityItem3;
+	
+	@FXML 
+	private TextField priceItem3;
+	
+	
+	@FXML 
+	private TextField totalPrice;
+	
+	@FXML 
+	private Button checkout;
+	
+	
+}
