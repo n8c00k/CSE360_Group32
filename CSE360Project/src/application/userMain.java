7425fd1944b923ea8478ae22f4a277ffcd130612
SopAlc23 <98788910+SopAlc23@users.noreply.github.com>
2022-03-25 19:49:20 -0700

    UserMain Buttons

---
+++ CSE360Project/src/application/userMain.java
@@ -0,0 +1,23 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+public class userMain {
+	public userMain() {
+		
+	}
+	
+	@FXML 
+	private Button menu;
+	
+	@FXML 
+	private Button account;
+	
+	@FXML 
+	private Button cart;
+	
+	@FXML 
+	private Button logOut;
+
+}
