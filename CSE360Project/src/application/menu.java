90072c6ece4093f45746d8283a85ce72fec4d90e
SopAlc23 <98788910+SopAlc23@users.noreply.github.com>
2022-03-25 20:53:46 -0700

    Buttons for Menu and Cart

--- CSE360Project/src/application/menu.java
+++ CSE360Project/src/application/menu.java
@@ -17,6 +17,7 @@ public class menu implements Initializable {
 	}
 	
 	
+	
 	@FXML
 	private Button search;
 	@FXML
@@ -27,7 +28,29 @@ public class menu implements Initializable {
 	private Button logout;
 	@FXML
 	private TextField searchItem;
-
+	@FXML
+	private Button addItem1;
+	@FXML
+	private Button addItem2;
+	@FXML
+	private Button addItem3;
+	@FXML
+	private Button addItem4;
+	@FXML
+	private Button addItem5;
+	
+	@FXML
+	private TextField qtyItem1;
+	@FXML
+	private TextField qtyItem2;
+	@FXML
+	private TextField qtyItem3;
+	@FXML
+	private TextField qtyItem4;
+	@FXML
+	private TextField qtyItem5;
+	
+	
 	public void initializeUser(User newUser) {
 		
 		user = newUser;
