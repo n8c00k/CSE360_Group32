89e3a6af21bed370a24ef043c961fc1e8d9dc631
SopAlc23 <98788910+SopAlc23@users.noreply.github.com>
2022-03-25 21:06:14 -0700

    Buttons in Account

--- CSE360Project/src/application/account.java
+++ CSE360Project/src/application/account.java
@@ -4,7 +4,9 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import application.dataObjects.User;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 
 public class account implements Initializable{
 	
@@ -12,8 +14,12 @@ public account() {
 		
 	}
 	
-
-
+	@FXML
+	private Button menu; 
+	@FXML
+	private Button cart;
+	@FXML
+	private Button logOut; 
 
 	private User user;
 	
