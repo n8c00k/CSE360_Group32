54ae9a63bc2360005b8d272af8a96938fee3054b
SopAlc23 <98788910+SopAlc23@users.noreply.github.com>
2022-03-29 20:54:52 -0700

    Coupon ID

--- CSE360Project/src/application/dataObjects/Coupon.java
+++ CSE360Project/src/application/dataObjects/Coupon.java
@@ -4,17 +4,27 @@ public class Coupon {
 
     public Boolean haveCoupon;
     public Integer userId;
+    public Integer Id;
 
 	public Coupon() {
 
 		}
+	
 	public Integer getUserId() {
     	return userId;
     }
+	
 	public void setUserId(Integer x){
     	this.userId = x;
     }
 	
+	public Integer getId() {
+    	return Id;
+    }
+	
+	public void setId(Integer x){
+    	this.Id = x;
+    }
 
 //    public static void validCoupon(){
 //        if(Customer.pastOrders == 5) {
