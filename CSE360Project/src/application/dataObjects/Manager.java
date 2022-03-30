0666a1f1a558ddb509131eb86502e91f685f5578
SopAlc23 <98788910+SopAlc23@users.noreply.github.com>
2022-03-30 16:26:57 -0700

    ArrayList for OrdersInProg

--- CSE360Project/src/application/dataObjects/Manager.java
+++ CSE360Project/src/application/dataObjects/Manager.java
@@ -1,14 +1,18 @@
 package application.dataObjects;
 
+import java.util.ArrayList;
 
 public class Manager extends User {
 
 	public Menu menu;
+	public ArrayList<Cart> ordersInProg;
 
 	public Manager( String name, String email, String password) {
 			super( name, email, password);
-			// TODO Auto-generated constructor stub
+			ordersInProg = new ArrayList<>();
 		}
+	
+	
 
     public void applyCoupon(Customer cust) {
 
