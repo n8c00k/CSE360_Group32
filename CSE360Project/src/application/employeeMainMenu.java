637c9db9019e9dc067b5ffa209cb6d8473c5da96
SopAlc23 <98788910+SopAlc23@users.noreply.github.com>
2022-04-01 15:58:05 -0700

    Creating Checkout & EmployeeMainMenu

---
+++ CSE360Project/src/application/employeeMainMenu.java
@@ -0,0 +1,19 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+
+public class employeeMainMenu implements Initializable {
+	public employeeMainMenu() {
+		
+	}
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
